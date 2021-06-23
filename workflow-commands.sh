f61055532012166a8ce3814e97101591255628fe
David Arctur <david.arctur@utexas.edu>
2021-06-23 14:10:15 -0500

    Update workflow-commands.sh
    minor edits

--- workflow-commands.sh
+++ workflow-commands.sh
@@ -2,6 +2,19 @@
 #   https://github.com/passaH2O/GeoFlood/Readme.md and 
 #   https://github.com/dhardestylewis/GeoFlood-Task_processor/blob/main/geoflood-task_processor/workflow_commands-geoflood_singularity.sh
 
+# start idev node and singularity support
+idev -N 1 -n 67 -t 2:00:00 -p development 	
+module load tacc-singularity
+ 
+## if needed, first remove then rebuild geoflood_docker_tacc.sif (or taudem) image in ${WORK2}
+cdw2
+singularity pull --disable-cache docker://dhardestylewis/geoflood_docker:tacc	
+singularity pull --disable-cache docker://dhardestylewis/taudem_docker:minimal   
+
+## only use singularity shell for GeoNet/GeoFlood commands, not TauDEM
+singularity shell ./geoflood_docker_tacc.sif	
+eval "$(conda shell.bash hook)"
+
 # Set tools and project environment names
 export WORKBASE='/work2/02044/arcturdk/stampede2'
 # export PROJECT='TX-Counties-Travis-120902050406'    ## HUC12 test dataset
@@ -19,6 +32,9 @@ export GEOINPUTS="${WORKING_DIR}/GeoInputs"
 export GEOOUTPUTS="${WORKING_DIR}/GeoOutputs"
 export LOCATION_NAME="${PROJECT}"
 
+# make sure conda env is set to geoflood except for grass step
+conda activate geoflood
+
 ## GeoNet - Configure and prepare file structure
 # ... override default input & output folder names
 # python ${GEOTOOLS}/GeoNet/pygeonet_configure.py -dir ${WORKING_DIR} -p ${PROJECT} -n ${PROJECT} --no_chunk --input_dir Inputs --output_dir Outputs 
@@ -28,14 +44,14 @@ python ${GEOTOOLS}/GeoNet/pygeonet_prepare.py
 
 # GeoNet steps 1-2. DEM smoothing, slope & curvature
 python ${GEOTOOLS}/GeoNet/pygeonet_nonlinear_filter.py
-python ${GEOTOOLS}/GeoNet/pygeonet_slope_curvature.py
+python ${GEOTOOLS}/GeoNet/pygeonet_slope_curvature.
 
 # GeoNet step 3. GRASS GIS
-# This needs to run in its own conda env
-conda deactivate geoflood
+# This needs to run in its own conda env, first deactivate geoflood
+conda deactivate 
 conda activate grass
-python ${GEOTOOLS}/GeoNet/pygeonet_grass_py3.py
-conda deactivate grass
+  python ${GEOTOOLS}/GeoNet/pygeonet_grass_py3.py
+conda deactivate 
 conda activate geoflood
 
 # GeoNet step 4. Flow accum & curvature skeleton
@@ -44,6 +60,7 @@ python ${GEOTOOLS}/GeoNet/pygeonet_skeleton_definition.py
 ## GeoFlood step 6. network node reading - make sure Catchment.shp, Flowline.shp are in ${GEOINPUTS}/GIS/${PROJECT}   
 python ${GEOTOOLS}/GeoFlood/Network_Node_Reading.py
 ### next edit the endPoints.csv file at "${GEOOUTPUTS}/GIS/${PROJECT}/${PROJECT}_endPoints.csv"
+cd ${GEOOUTPUTS}/GIS/${PROJECT}
 
 # GeoFlood steps 7. Negative HAND, and 8. Network Extraction
 python ${GEOTOOLS}/GeoFlood/Relative_Height_Estimation.py
